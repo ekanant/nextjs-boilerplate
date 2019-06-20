e4f96891d8feaaa900857cffd237f27a4cb9d5b4
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2019-06-20 07:57:38 +0700

    Add change language button

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-import { withTranslation, Link } from '../src/i18n'
+import { withTranslation, Link, i18n } from '../src/i18n'
 import React from 'react'
 
 import Head from '../components/head'
 
 const Homepage = (props) => {
+  let {t} = props
   return (
     <div>
       <Head title="Home" />
-
-      hello i18n=>{props.t('hello')}<br />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            {t('change_language')} <Link href='/th-th/'><a>TH</a></Link> | <Link href="/en-th/"><a>EN</a></Link>
+          </div>
+          <div className="col-12">
+            hello i18n=>{t('hello')}<br />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,4 @@
-const NextI18Next = require('next-i18next')
+const NextI18Next = require('next-i18next/dist/commonjs')
 
 const NextI18NextInstance = new NextI18Next({
   localeSubpaths: 'foreign',

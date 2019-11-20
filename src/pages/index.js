f001172137ee78152e3eacbb50b1b8e107dcb1d9
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2019-11-20 22:15:42 +0700

    Finish simple change language button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-import { withTranslation, Link, i18n } from '../i18n'
+import { withTranslation, i18n } from '../i18n'
+import Link from 'next/link'
 import React from 'react'
 import PropTypes from 'prop-types'
 
 
 import Head from '../components/head'
 
+const changeLanguage = (locale) => {
+  i18n.changeLanguage(locale)
+}
+
 const Homepage = (props) => {
-  let {t} = props
+  let { t } = props
   return (
     <div>
       <Head title="Home" />
       <div className="container">
         <div className="row">
           <div className="col-12">
-            {t('change_language')} <Link href='/th-th'><a>TH</a></Link> | <Link href="/en-th"><a>EN</a></Link>
+            {t('change_language')} <Link href={{ pathname: '/', query: {} }}><a onClick={(e) => {e.preventDefault();changeLanguage('th-th');}}>TH</a></Link> | <Link href={{ pathname: '/en-th', query: {} }}><a onClick={(e) => {e.preventDefault();changeLanguage('en-th');}}>EN</a></Link><br />
+
+            <div>
+              Current locale {i18n.language}
+            </div>
           </div>
           <div className="col-12">
             hello i18n=>{t('hello')}<br />

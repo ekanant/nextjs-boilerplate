0c15b6965a5fee7f76dac7f90d9bd8a8854df3b2
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-03-11 18:32:24 +0700

    Change from class style to functional

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import App from 'next/app'
 import { appWithTranslation } from '../i18n'
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
-  }
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
 }
 
 export default appWithTranslation(MyApp)

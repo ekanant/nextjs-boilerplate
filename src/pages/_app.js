5b6e3136445012d77988d09db3b274539fbfcd3e
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-05-16 22:44:52 +0700

    Change css link to inject in style tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import { appWithTranslation } from '../i18n'
 
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,34 @@
 // ./pages/_document.js
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import memoize from 'lodash.memoize'
+
+import { readFileSync } from 'fs';
+import {resolve} from 'path'
+
+const doGetContent = (file) => readFileSync(resolve(process.cwd(), ".next", file), "utf8");
+const getContent = process.env.NODE_ENV === "production" ? memoize(doGetContent) : doGetContent;
+
+class InlineStylesHead extends Head {
+  getCssLinks() {
+    return this.__getInlineStyles();
+  }
+
+  __getInlineStyles() {
+    const { assetPrefix, files } = this.context._documentProps;
+    if (!files || files.length === 0) return null;
+
+    return files.filter(file => /\.css$/.test(file)).map(file => (
+      <style
+        key={file}
+        data-href={`${assetPrefix}/_next/${file}`}
+        dangerouslySetInnerHTML={{
+          __html: getContent( file),
+        }}
+      />
+    ));
+  }
+}
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -14,9 +42,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
+        <InlineStylesHead>
           <style>{`body { margin: 0 } /* custom! */`}</style>
-        </Head>
+        </InlineStylesHead>
         <body className="custom_class">
           <Main />
           <NextScript />
